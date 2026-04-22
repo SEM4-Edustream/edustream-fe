a5f48b28c7f6fe2e99daf2f9587da0dd91997d19
hailongtn <hailong0309.tn@gmail.com>
2026-04-22 15:05:06 +0700

    feat(auth): install @supabase/supabase-js and configure client

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'i.pravatar.cc' },
       { protocol: 'https', hostname: 'link-anh-bia-demo.com' },
+      { protocol: 'https', hostname: 'edustream-video-storage-216261470374-ap-southeast-2-an.s3.ap-southeast-2.amazonaws.com' },
+      { protocol: 'https', hostname: '*.s3.ap-southeast-2.amazonaws.com' },
+      { protocol: 'https', hostname: '*.amazonaws.com' }
     ],
   },
 };
